78f2a723de6f69629789b4d61fe60dfd2da0ef72
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-10-08 23:12:12 +0200

    added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common';
 import { HelloModule } from './hello';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommonModule, HelloModule],
+  imports: [CommonModule, HelloModule, UserModule],
   controllers: [],
   providers: [],
 })
