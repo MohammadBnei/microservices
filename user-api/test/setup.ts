0cb1e4e100748146e28de7f0ed3ef2ded4b1d4e1
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-10-11 18:32:55 +0200

    let's go

--- user-api/test/setup.ts
+++ user-api/test/setup.ts
@@ -1,21 +1,16 @@
 import { MikroORM } from '@mikro-orm/core';
 import config from '../src/mikro-orm.config';
 import { UserSeeder } from './UserSeeder';
-import { config as dotenv } from 'dotenv';
-
-dotenv({
-  path: '.env.test',
-});
 
 let orm: MikroORM<any>;
 
 export default async () => {
   orm = await MikroORM.init({
     ...config,
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    dbName: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USER || 'nestjs',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    dbName: process.env.DATABASE_NAME || 'test',
     migrations: {
       allOrNothing: true,
     },
